7562b0a274d0f53a555d5acb40f62ccf1bb579db
nasrin haghighi <ns.haghighi@gmail.com>
2024-03-28 10:35:55 +0000

    fix:nextauth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,22 +1,13 @@
 import NextAuth from "next-auth/next"
 
-
+import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from "next-auth"
 
 //import prisma from "@/utils/connect"
 import { options } from "@/utils/auth"
 
-//
-// export default async function handler(req:any, res:any) {
-//     const getAuthSession = () => getServerSession(options)
-//     return NextAuth(req, res, options)
-//   }
 
-// export const getAuthSession = () => getServerSession(options)
-// const handler= NextAuth( options)
-// export {handler as GET, handler as POST}
+
 export const getAuthSession = () => getServerSession(options)
-
-const handler = NextAuth(options);
-
-export default handler;
+const handler= NextAuth( options)
+export {handler as GET, handler as POST}
