38abe5b314d25cc4369140f0f96ea0d5057193a6
nasrin haghighi <ns.haghighi@gmail.com>
2024-03-28 10:22:40 +0000

    fix:add eslint to next config, change in the netauth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,22 @@
 import NextAuth from "next-auth/next"
 
-import GoogleProvider from 'next-auth/providers/google'
-import  { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
+
 import { getServerSession } from "next-auth"
 
 //import prisma from "@/utils/connect"
 import { options } from "@/utils/auth"
 
+//
+// export default async function handler(req:any, res:any) {
+//     const getAuthSession = () => getServerSession(options)
+//     return NextAuth(req, res, options)
+//   }
 
-
-
+// export const getAuthSession = () => getServerSession(options)
+// const handler= NextAuth( options)
+// export {handler as GET, handler as POST}
 export const getAuthSession = () => getServerSession(options)
-const handler= NextAuth( options)
-export {handler as GET, handler as POST}
+
+const handler = NextAuth(options);
+
+export default handler;
