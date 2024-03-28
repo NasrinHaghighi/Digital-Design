7a282ed0e7b3e66b5be4aa601cdb83873fd4afc5
nasrin haghighi <ns.haghighi@gmail.com>
2024-03-28 12:22:17 +0000

    fix:remove unnesesry getauthsesion from post id rout

--- app/api/dashboard/post/[id]/route.ts
+++ app/api/dashboard/post/[id]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 //import { PrismaClient } from '@prisma/client';
-import { getAuthSession } from '../../../auth/[...nextauth]/route';
+
 import prisma from '../../../../../utils/connect'
 
 
